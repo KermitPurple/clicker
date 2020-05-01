99e5f3c18d6fc674daa22da7801a9386de437370
Shane McDonough <shanemcdman@gmail.com>
2020-04-30 21:34:30 -0700

    Now usable with any key

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import time
 import mouse
 import keyboard
 from colorama import Fore, init
+from sys import argv
 init(autoreset=True)
 
 def main():
     start_stop_key = 'ctrl+shift'
+    if len(argv) > 1:
+        arg = " ".join(argv[1:])
+        if arg == '-r':
+            press = lambda: mouse.right_click()
+            print(Fore.CYAN + "right click" + Fore.RESET + " selected")
+        else:
+            press = lambda: keyboard.send(arg)
+            print(Fore.CYAN + arg + Fore.RESET + " selected")
+    else:
+        press = lambda: mouse.click()
+        print(Fore.CYAN + "left click" + Fore.RESET + " selected")
     print("Press " + Fore.CYAN + start_stop_key + Fore.RESET + " to toggle clicker")
     while 1:
         print(Fore.RED + "Off")
@@ -13,7 +25,7 @@ def main():
         print(Fore.GREEN + "On")
         time.sleep(0.2)
         while 1:
-            mouse.click()
+            press()
             time.sleep(0.002)
             if keyboard.is_pressed(start_stop_key):
                 break
