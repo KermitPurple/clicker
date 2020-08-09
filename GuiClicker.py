f3c9a9f4ee793bec0866a16353ae16df687de9d5
Shane McDonough <shanemcdman@gmail.com>
2020-08-09 14:06:39 -0700

    Create basic clicker

--- GuiClicker.py
+++ GuiClicker.py
@@ -1,13 +1,32 @@
 import sys
 import threading
+import keyboard, mouse
 from gui import Ui_AutoClicker
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class GuiClicker(Ui_AutoClicker):
+    def __init__(self):
+        self.running = False
+
+    def setupUi(self, win):
+        Ui_AutoClicker.setupUi(self, win)
+        self.ToggleButton.clicked.connect(self.toggle_running)
+
+    def toggle_running(self):
+        self.running = not self.running
+        print(self.running)
+
     def run(self, win):
         while win.isActiveWindow():
-            print('1')
+            if self.running:
+                if self.LeftClick.isChecked():
+                    mouse.click()
+                    print("LeftClick")
+                elif self.RightClick.isChecked():
+                    mouse.right_click()
+                else:
+                    keyboard.send(self.PressText.text())
 
 def main():
     app = QApplication(sys.argv)
