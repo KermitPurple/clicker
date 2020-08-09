030c9ea1c7889054a266536f8007a2c8740833df
Shane McDonough <shanemcdman@gmail.com>
2020-08-09 13:50:53 -0700

    Close run when window closes

--- GuiClicker.py
+++ GuiClicker.py
@@ -1,10 +1,18 @@
 import sys
+import threading
 from gui import Ui_AutoClicker
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class GuiClicker(Ui_AutoClicker):
-    pass
+    def __init__(self):
+        self.running = True
+
+    def run(self, win):
+        while self.running:
+            print('h1')
+            if not win.isActiveWindow():
+                self.running = False
 
 def main():
     app = QApplication(sys.argv)
@@ -12,6 +20,8 @@ def main():
     gui = GuiClicker()
     gui.setupUi(win)
     win.show()
+    th = threading.Thread(target = gui.run, args=(win,))
+    th.start()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
